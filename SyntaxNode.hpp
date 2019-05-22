ecf3baec50ac9a0b6379c190e8bf5d40cb741e93
pinbraerts <shirshov.dima@gmail.com>
2019-05-22 15:04:23 +0300

    Add priority constructor

--- SyntaxNode.hpp
+++ SyntaxNode.hpp
@@ -11,6 +11,7 @@ struct SyntaxNode: Node, Token {
 	priority_t priority = 0;
 
 	SyntaxNode(TreePtr _parent, const Token& tok) noexcept: Token(tok), Node(_parent) {}
+	SyntaxNode(TreePtr _parent, const Token& tok, priority_t _priority) noexcept : Token(tok), Node(_parent), priority(_priority) {}
 	SyntaxNode(TreePtr _parent, Token::Type t) noexcept : Token(t), Node(_parent) {}
 	SyntaxNode(TreePtr _parent, Token::Type t, priority_t _priority) noexcept : Token(t), Node(_parent), priority(_priority) {}
 	SyntaxNode() noexcept : Token(Token::Error), Node(0) {} // for vector
