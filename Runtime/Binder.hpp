d740d62cccea69a16fdfa8485d34ee0268cf199b
pinbraerts <shirshov.dima@gmail.com>
2019-06-18 23:23:22 +0300

    Refactor binder

--- Runtime/Binder.hpp
+++ Runtime/Binder.hpp
@@ -20,48 +20,34 @@ struct Binder {
 		tree(_tree),
 		global(_global) {}
 
-	void BindSingle(TreePtr p) {
-		const SyntaxNode& node = syntax[p];
-		switch (node.type) {
-		case Token::Quote:
-			break;
-		case Token::String:
-			// TODO check for digit, think how to create and name temporary objects
-			tree.push_child(global.Find(std::string(node.view())));
-			break;
-		default: // skip
-			break;
-		}
-	}
-
-	Method* Find(std::string s) {
-		Method* m = global.Find(s);
+	Method* Find(std::string_view s) {
+		Method* m = global.Find(std::string(s));
 		if (m == nullptr)
-			return global.Declare<Method>(s);
+			return global.Declare<Method>(std::string(s));
 		return m;
 	}
-	Method* Find(const SyntaxNode& node) {
-		return Find(std::string(node.view()));
-	}
+	//Method* Find(const SyntaxNode& node) {
+	//	return Find(std::string_view(node));
+	//}
 	Method* Find(TreePtr p) {
-		return Find(std::string(syntax[p].view()));
+		return Find(syntax[p]);
 	}
 	Method* FindOperator(const SyntaxNode& node) {
-		return Find("operator"s + std::string(node.view()));
+		return Find("operator"s + std::string(node));
 	}
 	Method* FindOperator(TreePtr p) {
-		return Find("operator"s + std::string(syntax[p].view()));
+		return Find("operator"s + std::string(syntax[p]));
 	}
 
 	template<bool direction>
-	auto children_begin(const Children& c) {
+	auto ChildrenBegin(const Children& c) {
 		if constexpr (direction) {
 			return c.begin();
 		}
 		else return c.rbegin();
 	}
 	template<bool direction>
-	auto children_end(const Children& c) {
+	auto ChildrenEnd(const Children& c) {
 		if constexpr (direction) {
 			return c.end();
 		}
@@ -74,7 +60,7 @@ struct Binder {
 		const auto& c = node.children;
 		if (c.empty()) throw std::runtime_error("no no no");
 		
-		auto i = children_begin<direction>(c), e = children_end<direction>(c);
+		auto i = ChildrenBegin<direction>(c), e = ChildrenEnd<direction>(c);
 		BindUnaryOperator<direction>(p, i, e);
 		for (++i; i < e; ++i) {
 			if (syntax[*i].priority != 0) { // binary operator
@@ -83,11 +69,11 @@ struct Binder {
 				BindUnaryOperator<direction>(p, i, e);
 				if constexpr (direction) {
 					auto ce = tree.last().children.end();
-					tree.insert_capture(ce - 2, ce, op);
+					tree.InsertAndCapture(ce - 2, ce, op);
 				}
 				else {
 					auto cb = tree.last().children.begin();
-					tree.insert_capture(cb, cb + 2, op);
+					tree.InsertAndCapture(cb, cb + 2, op);
 				}
 			}
 		}
@@ -112,9 +98,9 @@ struct Binder {
 			--i;
 			Method* op = FindOperator(*i);
 			if constexpr(direction)
-				tree.insert_capture(tree.last().children.end() - 1, tree.last().children.end(), op);
+				tree.InsertAndCapture(tree.last().children.end() - 1, tree.last().children.end(), op);
 			else
-				tree.insert_capture(tree.last().children.begin(), tree.last().children.begin() + 1, op);
+				tree.InsertAndCapture(tree.last().children.begin(), tree.last().children.begin() + 1, op);
 		}
 	}
 
@@ -135,7 +121,7 @@ struct Binder {
 		if (syntax[*i].priority != node.priority)
 			throw std::runtime_error("no no no"); // TODO think about function call
 		Method* op = FindOperator(*i);
-		tree.select(tree.insert_capture(tree.last().children.end() - 1, tree.last().children.end(), op));
+		tree.Select(tree.InsertAndCapture(tree.last().children.end() - 1, tree.last().children.end(), op));
 		for (++i; i < e; ++i) {
 			if (syntax[*i].priority == node.priority)
 				throw std::runtime_error("empty space");
@@ -145,7 +131,7 @@ struct Binder {
 			if (syntax[*i].priority != node.priority)
 				throw std::runtime_error("no no no"); // TODO think about function call
 		}
-		tree.up();
+		tree.GoUp();
 	}
 
 	void BindOperator(TreePtr p) {
@@ -174,13 +160,13 @@ struct Binder {
 			node.type == Token::CloseBrace ||
 			node.type == Token::OpenBrace) return;
 		else if (node.children.empty() || (node.children.size() == 1 && syntax[node.children[0]].type == Token::Space)) {
-			tree.push_child(Find(node));
+			tree.PushChild(Find(node));
 		}
 		else if(node.priority == 0) { // TODO check if it's a braces or string literal or root element
-			tree.push_child_and_select(Find("Braces"));
+			tree.PushChildAndSelect(Find("Braces"));
 			for (TreePtr i : node.children)
 				Bind(i);
-			tree.up();
+			tree.GoUp();
 		}
 		else if (node.priority < 100) { // operator
 			BindOperator(p);
--- Runtime/main.cpp
+++ Runtime/main.cpp
@@ -10,10 +10,10 @@ struct Operation {
 		Call,
 		Push,
 	} code;
-
 };
 
 int main() {
+#if 0
 	Scope global;
 
 	Type* cint = global.Declare<Type>("Int");
@@ -26,5 +26,22 @@ int main() {
 	std::cout << std::boolalpha;
 	std::cout << _X->Find("a")->type->name << std::endl;
 	std::cout << bool(_X->Find("c")) << std::endl;
+#else
+	Lexer l = Lexer::FromFile("..//Analysis//example.fc");
+	SyntaxTree tree;
+	Parser p(tree, l);
+	Parser::Error e = p.Parse();
+	if (!e.is_ok())
+		std::cerr << e.message << std::endl;
+	std::cout << tree << std::endl;
+	std::ofstream output("..//Analysis//example.out", std::ios::binary);
+	tree.SaveBinary(output, l.source.begin().ptr());
+
+	Scope global;
+	BoundTree b;
+	Binder(tree, b, global).Bind();
+	std::cout << b << std::endl;
+
+#endif
 	return 0;
 }
